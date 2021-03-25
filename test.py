1187c45280f9827d8a1c943e39317352a509ed6e
emeka <emekajohnobi@Emekas-MacBook-Air.local>
2021-03-25 11:40:47 -0500

    I just added test.py

--- test.py
+++ test.py
@@ -1,20 +1,35 @@
-def canPlaceFlowers(flowerbed,  n):
-        count = 0
-        for i in flowerbed:
-            if i == 0:
-                count +=1 
+# # def canPlaceFlowers(flowerbed,  n):
+# #         count = 0
+# #         for i in flowerbed:
+# #             if i == 0:
+# #                 count +=1 
 
 
-        sum1= 3
-        for i in range(2,n+1):
-            sum1 += 2 
-        print(sum1)
+# #         sum1= 3
+# #         for i in range(2,n+1):
+# #             sum1 += 2 
+# #         print(sum1)
             
-        if count < sum1:
-            return False
-        else:
-            return True
+# #         if count < sum1:
+# #             return False
+# #         else:
+# #             return True
             
 
 
-print(canPlaceFlowers([1,0,0,0,0,0,0,0,1], 3))
+# # print(canPlaceFlowers([1,0,0,0,0,0,0,0,1], 3))
+
+
+
+# # def consistent_strings(allowed, words):
+
+# x = 17875865859
+# print(sum(str(x)))
+
+x = [1,2,3,4]
+for i in range(len(x)):
+    if x[i] == 1:
+        x[i] = 2
+
+print(x)
+        
